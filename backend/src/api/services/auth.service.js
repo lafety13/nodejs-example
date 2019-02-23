d563bb3d2bcef3cb9cb844bfae010b44028cbd11
Vadym Kozak <vadim.kozak.it@gmail.com>
2019-02-23 21:29:36 +0200

    code cleanup

--- backend/src/api/services/auth.service.js
+++ backend/src/api/services/auth.service.js
@@ -9,15 +9,11 @@ const { RefreshTokenModel } = require('../models/refreshToken.model');
 const { UserModel } = require('../models/user.model');
 
 const _handleTokenProcess = async (user) => {
-    try {
-        const tokenType = 'Bearer';
-        const accessToken = await _generateToken(user);
-        const { token } = await _buildRefreshTokenData(user);
+    const tokenType = 'Bearer';
+    const accessToken = await _generateToken(user);
+    const { token } = await _buildRefreshTokenData(user);
 
-        return { tokenType, accessToken, refreshToken: token, user };
-    } catch (error) {
-        throw error;
-    }
+    return { tokenType, accessToken, refreshToken: token, user };
 };
 
 const _generateToken = (user) => {
@@ -29,97 +25,77 @@ const _generateToken = (user) => {
 };
 
 const _buildRefreshTokenData = async (user) => {
-    try {
-        const [ newRefreshToken, refreshToken ] = await Promise.all([
-            bcrypt.hash(user.id.toString(), Math.random()),
-            RefreshTokenModel.findOne({ userId: user.id })
-        ]);
+    const [ newRefreshToken, refreshToken ] = await Promise.all([
+        bcrypt.hash(user.id.toString(), Math.random()),
+        RefreshTokenModel.findOne({ userId: user.id })
+    ]);
 
-        const tokenData = {
-            expires: moment().add(30, 'days').unix(),
-            token: newRefreshToken
-        };
+    const tokenData = {
+        expires: moment().add(30, 'days').unix(),
+        token: newRefreshToken
+    };
 
-        let refreshTokenModel;
-        if (refreshToken) {
-            refreshTokenModel = await refreshToken
-                .update(tokenData)
-                .then(() => ({
-                    ...refreshToken.toObject(),
-                    ...tokenData
-                }));
-        } else {
-            const model = new RefreshTokenModel({
-                userId: user.id,
-                userEmail: user.email,
+    let refreshTokenModel;
+    if (refreshToken) {
+        refreshTokenModel = await refreshToken
+            .update(tokenData)
+            .then(() => ({
+                ...refreshToken.toObject(),
                 ...tokenData
-            });
-            refreshTokenModel = await model.save();
-        }
-
-        return refreshTokenModel;
-    } catch (error) {
-        throw error;
+            }));
+    } else {
+        const model = new RefreshTokenModel({
+            userId: user.id,
+            userEmail: user.email,
+            ...tokenData
+        });
+        refreshTokenModel = await model.save();
     }
+
+    return refreshTokenModel;
 };
 
 exports.login = async (credentials) => {
-    try {
-        const user = await UserModel.findOne({ email: credentials.email });
+    const user = await UserModel.findOne({ email: credentials.email });
 
-        if (!user || !user.checkPassword(credentials.password)) {
-            throw new AuthError({
-                message: 'Password or/and email is invalid.',
-                status: httpStatus.UNPROCESSABLE_ENTITY
-            });
-        }
-
-        return await _handleTokenProcess(user);
-    } catch (error) {
-        throw error;
+    if (!user || !user.checkPassword(credentials.password)) {
+        throw new AuthError({
+            message: 'Password or/and email is invalid.',
+            status: httpStatus.UNPROCESSABLE_ENTITY
+        });
     }
+
+    return await _handleTokenProcess(user);
 };
 
 exports.logout = async (userId) => {
-    try {
-        return await RefreshTokenModel.findOneAndDelete({ userId });
-    } catch (error) {
-        throw error;
-    }
+    return await RefreshTokenModel.findOneAndDelete({ userId });
 };
 
 exports.register = async (userBody) => {
-    try {
-        const user = new UserModel(userBody);
-        return await user.save();
-    } catch (error) {
-        throw error;
-    }
+    const user = new UserModel(userBody);
+    return await user.save();
 };
 
 exports.refresh = async (refreshToken, userId) => {
-    try {
-        const [ user, refreshModel ] = await Promise.all([
-            UserModel.findOne({ _id: userId }),
-            RefreshTokenModel.findOne({ userId })
-        ]);
+    const [ user, refreshModel ] = await Promise.all([
+        UserModel.findOne({ _id: userId }),
+        RefreshTokenModel.findOne({ userId })
+    ]);
 
-        if (!refreshModel && refreshModel.token !== refreshToken) {
-            throw new AuthError({
-                message: 'Invalid token',
-                status: httpStatus.UNAUTHORIZED
-            });
-        }
-
-        if (moment(refreshModel.expires).diff(moment(), 'seconds') < 0) {
-            throw new AuthError({
-                message: 'Refresh token is expired',
-                status: httpStatus.UNAUTHORIZED
-            });
-        }
-
-        return _handleTokenProcess(user);
-    } catch (error) {
-        throw error;
+    if (!refreshModel && refreshModel.token !== refreshToken) {
+        throw new AuthError({
+            message: 'Invalid token',
+            status: httpStatus.UNAUTHORIZED
+        });
     }
+
+    if (moment(refreshModel.expires).diff(moment(), 'seconds') < 0) {
+        throw new AuthError({
+            message: 'Refresh token is expired',
+            status: httpStatus.UNAUTHORIZED
+        });
+    }
+
+    return _handleTokenProcess(user);
 };
--- backend/src/api/services/user.service.js
+++ backend/src/api/services/user.service.js
@@ -1,11 +1,5 @@
 const { UserModel } = require('../models/user.model');
-const logger = require('../../config/logger');
 
 exports.findUserById = async (id) => {
-    try {
-        return await UserModel.findOne({ _id: id });
-    } catch (e) {
-        logger.error(e);
-        throw e;
-    }
+    return await UserModel.findOne({ _id: id });
 };
